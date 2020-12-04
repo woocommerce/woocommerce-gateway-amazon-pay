9518553f52a7d2e9e0d65ea5e62be6b14ba6369f
Matias Saggiorato <matiassaggiorato@gmail.com>
2020-12-04 15:33:15 -0300

    Allow multiple buttons to be initialized individually.

--- assets/js/amazon-wc-checkout.js
+++ assets/js/amazon-wc-checkout.js
@@ -1,27 +1,38 @@
 /*global jQuery, window, document, setTimeout, console, amazon_payments_advanced, amazon */
 ( function( $ ) {
 	$( function() {
+		var button_count = 0;
 		var button_id = '#pay_with_amazon';
 		function renderButton() {
 			attemptRefreshData();
 			if ( 0 === $( button_id ).length ) {
 				return;
 			}
-			var separator_id = '.wc-apa-button-separator';
-			var button_settings = {
-				// set checkout environment
-				merchantId: amazon_payments_advanced.merchant_id,
-				ledgerCurrency: 'EUR',
-				sandbox: amazon_payments_advanced.sandbox === '1' ? true : false,
-				// customize the buyer experience
-				productType: amazon_payments_advanced.action,
-				placement: amazon_payments_advanced.placement,
-				buttonColor: amazon_payments_advanced.button_color,
-				// configure Create Checkout Session request
-				createCheckoutSessionConfig: amazon_payments_advanced.create_checkout_session_config
-			};
-			amazon.Pay.renderButton( button_id, button_settings );
-			$( button_id ).siblings( separator_id ).show();
+			button_count++;
+			$( button_id ).each( function() {
+				var thisButton = $( this );
+				var thisId = thisButton.attr( 'id' );
+				if ( typeof thisId === 'undefined' ) {
+					thisId = 'pay_with_amazon_' + button_count;
+					thisButton.attr( 'id', thisId );
+				}
+				thisId = '#' + thisId;
+				var separator_id = '.wc-apa-button-separator';
+				var button_settings = {
+					// set checkout environment
+					merchantId: amazon_payments_advanced.merchant_id,
+					ledgerCurrency: 'EUR',
+					sandbox: amazon_payments_advanced.sandbox === '1' ? true : false,
+					// customize the buyer experience
+					productType: amazon_payments_advanced.action,
+					placement: amazon_payments_advanced.placement,
+					buttonColor: amazon_payments_advanced.button_color,
+					// configure Create Checkout Session request
+					createCheckoutSessionConfig: amazon_payments_advanced.create_checkout_session_config
+				};
+				amazon.Pay.renderButton( thisId, button_settings );
+				thisButton.siblings( separator_id ).show();
+			} );
 		}
 		renderButton();
 		$( document.body ).on( 'updated_wc_div', renderButton );
