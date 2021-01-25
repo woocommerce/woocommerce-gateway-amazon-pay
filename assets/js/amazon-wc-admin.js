8203f6b5f2772ab08a20f053afdb1c9188dfec53
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-01-25 16:53:49 -0300

    Make function to rebuild dropdowns reusable

--- assets/js/amazon-wc-admin.js
+++ assets/js/amazon-wc-admin.js
@@ -4,7 +4,6 @@
 		simple_path_form_id: 'simple_path',
 		payment_region_input: $( '#woocommerce_amazon_payments_advanced_payment_region' ),
 		button_language_input: $( '#woocommerce_amazon_payments_advanced_button_language' ),
-		button_languages: {},
 		action_url: '#',
 		spId: '',
 		register_now_link: $( 'a.register_now' ),
@@ -23,11 +22,40 @@
 		poll_timer: false,
 		poll_interval: 3000,
 		main_setting_form: $( '#mainform' ),
-		init_settings: function() {
-			wc_simple_path_form.button_language_input.children( 'option' ).each( function() {
+		init_dynamic_options: function( select, parent, combos ) {
+			var allOptions = select.data( 'wc-apa-all-options' );
+			if ( typeof allOptions !== 'undefined' ) {
+				return allOptions;
+			}
+			allOptions = {};
+			select.data( 'wc-apa-all-options', allOptions );
+
+			select.children( 'option' ).each( function() {
 				var key = $( this ).prop( 'value' ).replace( '-', '_' );
-				wc_simple_path_form.button_languages[ key ] = $( this ).text();
+				allOptions[ key ] = $( this ).text();
 			} );
+
+			var watch = function() {
+				var val = parent.val();
+				wc_simple_path_form.select_rebuild( select, combos, val );
+			};
+
+			watch();
+
+			parent.on( 'change', watch );
+
+			return allOptions;
+		},
+		init_settings: function() {
+			$.each( amazon_admin_params.language_combinations, function( i, langs ) {
+				langs.unshift( '' );
+				langs = $.map( langs, function( item ) {
+					return item.replace( '-', '_' );
+				} );
+				amazon_admin_params.language_combinations[ i ] = langs;
+			} );
+
+			wc_simple_path_form.init_dynamic_options( wc_simple_path_form.button_language_input, wc_simple_path_form.payment_region_input, amazon_admin_params.language_combinations );
 
 			// Init values if region is already selected
 			wc_simple_path_form.payment_region_on_change();
@@ -38,19 +66,20 @@
 			wc_simple_path_form.delete_settings_link.on( 'click', this.delete_settings_on_click );
 			$( document ).on( 'click', 'a.wcapa-toggle-section', this.toggle_visibility );
 		},
-		payment_button_language_rebuild: function() {
-			var langs = amazon_admin_params.language_combinations[ wc_simple_path_form.get_region_selected() ].slice();
-			langs.unshift( '' );
-			langs = $.map( langs, function( item ) {
-				return item.replace( '-', '_' );
-			} );
-			var selected = wc_simple_path_form.button_language_input.children( 'option:selected' );
-			var selected_key = selected.prop( 'value' ).replace( '-', '_' );
+		select_rebuild: function( select, combos, val ) {
+			var allOptions = select.data( 'wc-apa-all-options' );
+			var langs = combos[ val ];
+			if ( typeof combos[ val ] === 'boolean' ) {
+				langs = combos[ val ] = Object.keys( allOptions );
+			}
+			langs = langs.slice();
+			var selected = select.children( 'option:selected' );
+			var selected_key = selected.prop( 'value' );
 			if ( langs.indexOf( selected_key ) === -1 ) {
-				wc_simple_path_form.button_language_input.children( 'option' ).remove();
+				select.children( 'option' ).remove();
 				selected_key = false;
 			} else {
-				wc_simple_path_form.button_language_input.children( 'option' ).not( ':selected' ).remove();
+				select.children( 'option' ).not( ':selected' ).remove();
 			}
 			var found = false;
 			$.each( langs, function( i, key ) {
@@ -62,22 +91,21 @@
 				var newOpt = $(
 					'<option/>',
 					{
-						value: key.replace( '_', '-' ),
+						value: key,
 					}
-				).html( wc_simple_path_form.button_languages[ key ] );
+				).html( allOptions[ key ] );
 
 				if ( selected_key && ! found ) {
 					selected.before( newOpt );
 				} else {
-					wc_simple_path_form.button_language_input.append( newOpt );
+					select.append( newOpt );
 				}
 			} );
 			if ( ! selected_key ) {
-				wc_simple_path_form.button_language_input.children().first().prop( 'selected', true );
+				select.children().first().prop( 'selected', true );
 			}
 		},
 		payment_region_on_change: function() {
-			wc_simple_path_form.payment_button_language_rebuild();
 			if ( 'jp' === wc_simple_path_form.get_region_selected() ) {
 				// JP does not have Simple Path Registration, we open a new url for it.
 				wc_simple_path_form.register_now_link.attr( 'href', wc_simple_path_form.get_simple_path_url() );
