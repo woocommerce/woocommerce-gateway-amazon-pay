e4dde333b84fc6529e6682ddf11ab5db85d5862c
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-01-28 10:32:51 -0300

    Improve logging for refund_charge

--- includes/class-wc-amazon-payments-advanced-api.php
+++ includes/class-wc-amazon-payments-advanced-api.php
@@ -483,6 +483,7 @@ class WC_Amazon_Payments_Advanced_API extends WC_Amazon_Payments_Advanced_API_Ab
 		if ( ! is_null( $amount ) ) {
 			$data['refundAmount']['amount'] = $amount;
 		}
+		wc_apa()->log( __METHOD__, sprintf( 'Charge ID %s.', $charge_id ), $data );
 
 		$result = $client->createRefund(
 			$data,
@@ -494,8 +495,11 @@ class WC_Amazon_Payments_Advanced_API extends WC_Amazon_Payments_Advanced_API_Ab
 		$response = json_decode( $result['response'] );
 
 		if ( ! isset( $result['status'] ) || ! in_array( $result['status'], array( 200, 201 ), true ) ) {
+			wc_apa()->log( __METHOD__, sprintf( 'ERROR. Charge ID %s.', $charge_id ), $result );
 			return new WP_Error( $response->reasonCode, $response->message ); // phpcs:ignore WordPress.NamingConventions
 		}
+
+		wc_apa()->log( __METHOD__, sprintf( 'SUCCESS. Charge ID %s.', $charge_id ), $response );
 
 		return $response;
 	}
