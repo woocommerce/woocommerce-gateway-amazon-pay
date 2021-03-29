c34a7fcdd29f1206223f88eab28c1c0f521d4555
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-03-29 16:24:50 -0300

    Fix fatal introduced when removing class

--- woocommerce-gateway-amazon-payments-advanced.php
+++ woocommerce-gateway-amazon-payments-advanced.php
@@ -151,7 +151,6 @@ class WC_Amazon_Payments_Advanced {
 		include_once $this->includes_path . 'class-wc-amazon-payments-advanced-ipn-handler-abstract.php';
 		include_once $this->includes_path . 'class-wc-amazon-payments-advanced-ipn-handler.php';
 		include_once $this->includes_path . 'legacy/class-wc-amazon-payments-advanced-ipn-handler-legacy.php';
-		include_once $this->includes_path . 'class-wc-amazon-payments-advanced-synchronous-handler.php';
 
 		// On install hook.
 		include_once $this->includes_path . 'class-wc-amazon-payments-advanced-install.php';
