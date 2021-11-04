88b35f88a03a70e4419e4d3399474ab84fb189f3
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-11-04 17:07:25 -0300

    Fix function call.

--- includes/class-wc-gateway-amazon-payments-advanced-abstract.php
+++ includes/class-wc-gateway-amazon-payments-advanced-abstract.php
@@ -949,7 +949,7 @@ abstract class WC_Gateway_Amazon_Payments_Advanced_Abstract extends WC_Payment_G
 	 * @return bool
 	 */
 	public function has_other_gateways_enabled() {
-		$gateways = WC()->payment_gateways->get_available_payment_gateways();
+		$gateways = WC()->payment_gateways()->get_available_payment_gateways();
 		unset( $gateways['amazon_payments_advanced'] );
 		if ( ! empty( $gateways ) ) {
 			foreach ( $gateways as $gateway ) {
