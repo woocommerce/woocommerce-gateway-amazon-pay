ea631f1f76fbb1505374a62fbad44b032c512ebf
dpanta <dimitrios@saucal.com>
2022-05-24 21:41:09 +0300

    CR amends

--- includes/class-wc-gateway-amazon-payments-advanced.php
+++ includes/class-wc-gateway-amazon-payments-advanced.php
@@ -2813,7 +2813,12 @@ class WC_Gateway_Amazon_Payments_Advanced extends WC_Gateway_Amazon_Payments_Adv
 		$phone_number = $order->get_shipping_phone();
 		$phone_number = $phone_number ? $phone_number : $order->get_billing_phone();
 
-		$shipping_state = $order->get_shipping_state();
+		$shipping_state   = $order->get_shipping_state();
+		$shipping_country = $order->get_shipping_country( 'edit' );
+
+		if ( 'JP' === strtoupper( $shipping_country ) && 'JP' === strtoupper( WC_Amazon_Payments_Advanced_API::get_region() ) && isset( self::JP_REGION_CODE_MAP[ $shipping_state ] ) ) {
+			$shipping_state = self::JP_REGION_CODE_MAP[ $shipping_state ];
+		}
 
 		$payload['addressDetails'] = array(
 			'name'          => rawurlencode( $order->get_shipping_first_name() . ' ' . $order->get_shipping_last_name() ),
@@ -2822,13 +2827,9 @@ class WC_Gateway_Amazon_Payments_Advanced extends WC_Gateway_Amazon_Payments_Adv
 			'city'          => rawurlencode( $order->get_shipping_city() ),
 			'stateOrRegion' => rawurlencode( $shipping_state ),
 			'postalCode'    => rawurlencode( $order->get_shipping_postcode() ),
-			'countryCode'   => $order->get_shipping_country( 'edit' ),
+			'countryCode'   => $shipping_country,
 			'phoneNumber'   => rawurlencode( $phone_number ),
 		);
-
-		if ( 'JP' === strtoupper( $payload['addressDetails']['countryCode'] ) && 'JP' === strtoupper( WC_Amazon_Payments_Advanced_API::get_region() ) && isset( self::JP_REGION_CODE_MAP[ $shipping_state ] ) ) {
-			$payload['addressDetails']['stateOrRegion'] = rawurlencode( self::JP_REGION_CODE_MAP[ $shipping_state ] );
-		}
 
 		/**
 		 * Address Validation for the EU region.
