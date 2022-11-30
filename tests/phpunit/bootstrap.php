c8c43fb8b76ca0a178841c82a7c0a3d720183fc7
Dimitrios Pantazis <dpanta94@gmail.com>
2022-11-30 16:37:29 +0200

    [wip] unit tests

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -11,7 +11,7 @@ if ( ! $_tests_dir ) {
 }
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run tests/bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	echo "Could not find $_tests_dir/includes/functions.php, have you run tests/bin/install-phpunit-tests-dependencies.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
 }
 
@@ -32,41 +32,42 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	$_plugins_dir = dirname( __FILE__ ) . '/../../../';
 
-	// require_once $_plugins_dir . 'woocommerce/woocommerce.php';
+	require_once $_plugins_dir . 'woocommerce/woocommerce.php';
 
 	require_once $_plugins_dir . 'woocommerce-gateway-amazon-payments-advanced/woocommerce-gateway-amazon-payments-advanced.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-tests_add_filter(
-	'shutdown',
-	function() {
-		global $wpdb;
+// tests_add_filter(
+// 	'shutdown',
+// 	function() {
+// 		global $wpdb;
 
-		$tables = array_merge(
-			array_values( $wpdb->tables() ),
-			array(
-				$wpdb->prefix . 'woocommerce_api_keys',
-				$wpdb->prefix . 'woocommerce_attribute_taxonomies',
-				$wpdb->prefix . 'woocommerce_downloadable_product_permissions',
-				$wpdb->prefix . 'woocommerce_order_items',
-				$wpdb->prefix . 'woocommerce_payment_tokens',
-				$wpdb->prefix . 'woocommerce_payment_tokens',
-				$wpdb->prefix . 'woocommerce_shipping_zone_locations',
-				$wpdb->prefix . 'woocommerce_sessions',
-				$wpdb->prefix . 'woocommerce_shipping_zones',
-				$wpdb->prefix . 'woocommerce_shipping_zone_methods',
-				$wpdb->prefix . 'woocommerce_tax_rates',
-				$wpdb->prefix . 'woocommerce_tax_rate_locations',
-			)
-		);
+// 		$tables = array_merge(
+// 			array_values( $wpdb->tables() ),
+// 			array(
+// 				$wpdb->prefix . 'woocommerce_api_keys',
+// 				$wpdb->prefix . 'woocommerce_attribute_taxonomies',
+// 				$wpdb->prefix . 'woocommerce_downloadable_product_permissions',
+// 				$wpdb->prefix . 'woocommerce_order_items',
+// 				$wpdb->prefix . 'woocommerce_payment_tokens',
+// 				$wpdb->prefix . 'woocommerce_payment_tokens',
+// 				$wpdb->prefix . 'woocommerce_shipping_zone_locations',
+// 				$wpdb->prefix . 'woocommerce_sessions',
+// 				$wpdb->prefix . 'woocommerce_shipping_zones',
+// 				$wpdb->prefix . 'woocommerce_shipping_zone_methods',
+// 				$wpdb->prefix . 'woocommerce_tax_rates',
+// 				$wpdb->prefix . 'woocommerce_tax_rate_locations',
+// 				$wpdb->prefix . 'wc_webhooks',
+// 			)
+// 		);
 
-		foreach ( $tables as $table ) {
-			$wpdb->query( "DROP TABLE IF EXISTS $table;" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-		}
-	},
-	9999
-);
+// 		foreach ( $tables as $table ) {
+// 			$wpdb->query( "DROP TABLE IF EXISTS $table;" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+// 		}
+// 	},
+// 	9999
+// );
 
 require $_tests_dir . '/includes/bootstrap.php';
