2fe704204436e68a85f5b84d44294fe547feb2a1
Alejandro Iglesias <alexiglesias3110@gmail.com>
2024-03-05 17:42:14 +0100

    use "-" instead of "." as fallback

--- includes/class-wc-amazon-payments-advanced-api-abstract.php
+++ includes/class-wc-amazon-payments-advanced-api-abstract.php
@@ -391,7 +391,7 @@ abstract class WC_Amazon_Payments_Advanced_API_Abstract {
 			);
 		}
 		// Use fallback value for the last name to avoid field required errors.
-		$last_name_fallback = '.';
+		$last_name_fallback = '-';
 		$names              = explode( ' ', $name );
 		return array(
 			'first_name' => array_shift( $names ),
